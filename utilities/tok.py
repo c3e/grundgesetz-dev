2e2c1bd9f1803377850c3157263c5dea707a2d19
Bernhard Fisseni <bernhard.fisseni@googlemail.com>
2013-01-20 21:00:15 +0100

    first ‘semantic’ tree

--- utilities/tok.py
+++ utilities/tok.py
@@ -5,6 +5,7 @@
 
 from codecs import open
 import os
+import sys
 from collections import OrderedDict
 # from glob import glob
 
@@ -12,10 +13,13 @@ from difflib import SequenceMatcher
 
 import regex
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 
 numbered_re = regex.compile(ur'^(?:(?:\d+|[XVI]+)[a-z]\.)')
 
+
+def is_numbered(s):
+    return numbered_re.search(s)
 
 
 class Item:
@@ -64,7 +68,7 @@ class HierItem:
 
     def is_repealed(self):
         if len(self.children) == 1 and \
-            self.children[0].p == u"[aufgehoben]":
+            self.children[0].p.text == u"[aufgehoben]":
                 return True
         return False
 
@@ -183,6 +187,45 @@ def soupify_file(fn, v):
     return BeautifulSoup(open(fn_akt, encoding="utf-8"))
 
 
+tag_hierarchy = [
+     "h1",
+     "h2",
+     # "ol", # not necessary as this is already a tree
+]
+
+
+def arborify(soup):
+    u'''semanticise markup: document tree from tag soup'''
+    cur_el = soup.h1
+    tag_stack = []
+    div_stack = [BeautifulSoup()]
+    # loop over elements and build div-tree
+    while cur_el is not None:  # Warning: cur_el may be non-element!
+        next_el = cur_el.next_sibling
+        if type(cur_el) == element.Tag and cur_el.name in tag_hierarchy:
+            if tag_stack:
+                hier = tag_hierarchy.index(tag_stack[-1].name)
+            else:  # trick für initial element
+                hier = -1
+            cur_hier = tag_hierarchy.index(cur_el.name)
+            if tag_stack:
+                if hier >= cur_hier:
+                    # could we have a tag_stack == ["h1", "ol"]?
+                    # should not, but we can take care of it:
+                    while tag_stack and tag_hierarchy.index(tag_stack[-1].name) >= cur_hier:
+                        tag_stack.pop()
+                        div_stack.pop()
+            tag_stack.append(cur_el)
+            div_stack.append(soup.new_tag("div", level=cur_hier))
+            div_stack[-2].append(div_stack[-1])
+            div_stack[-1].insert_after("\n")
+        if div_stack:
+            div_stack[-1].append(cur_el)
+        else:
+            sys.stderr.write("Dangling text!? {}".format(unicode(cur_el)))
+        cur_el = next_el
+    return div_stack[0]
+
 f_toks = []
 
 version_trees = {}
@@ -191,7 +234,8 @@ for v in versionen:
     for fn in fn_s:
         print "V: ", v
         soup = soupify_file(fn, v)
-        version_trees = [Section(a) for a in soup("h1")]
+        with open("GG_%02d.html"%v, "w") as gg:
+            gg.write(unicode(arborify(soup)))
 
         # f_toks.append(tokenise_file(fn, v))
 
